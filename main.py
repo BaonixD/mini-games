6b714a98354c984e8c8e01a3d5ce5bdc7d6c79e6
Ilyas <amzehanilias@gmail.com>
2025-03-30 21:35:15 +0500

    начало

---
+++ main.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+
+
+class Container (BoxLayout):
+    pass
+
+
+class MyApp (App):
+    def build (self):
+        return Container()
+    
+    
+if __name__ == '__main__':
+    MyApp().run()
+    
